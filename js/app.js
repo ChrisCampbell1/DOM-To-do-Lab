0872c9fd2aa1af13588fb01adf8ead116b4e46a3
Chris Campbell <christopherecampbell@gmail.com>
2022-12-06 21:29:03 -0700

    add cashed elements and event listener

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("sanity check")
+//cashed elements
+const inputElement = document.querySelector("#input")
+const buttonElement = document.querySelector("#submit-button")
+const ulElement = document.querySelector("#todo-list")
+
+//console.log tests
+console.log(inputElement)
+
+
+//event listeners
+buttonElement.addEventListener("click", function(){
+  const liElement = document.createElement("li")
+  liElement.textContent = inputElement.value
+  ulElement.appendChild(liElement)
+  inputElement.value = ""
+})
