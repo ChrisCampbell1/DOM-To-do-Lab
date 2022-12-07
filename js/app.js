22e92b035b422e78194cecf3cf8724a7095face0
Chris Campbell <christopherecampbell@gmail.com>
2022-12-07 11:53:16 -0700

    working on deleting items on click

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ const inputElement = document.querySelector("#input")
 const buttonElement = document.querySelector("#submit-button")
 const ulElement = document.querySelector("#todo-list")
 const resetElement = document.querySelector("#reset-button")
-//const todoItem = document.querySelectorAll(".todo-item")
+const todoItems = document.querySelectorAll(".todo-item")
+
 
 //console.log tests
-console.log()
+//console.log(todoItems)
 
 
 //event listeners
@@ -24,8 +25,3 @@ resetElement.addEventListener("click", function(){
   ulElement.innerHTML = ""
   inputElement.value = ""
 })
-
-// can't get individual LIs to respond to the event listener, only the whole UL
-// ulElement.addEventListener("click", function(){
-//   ulElement.child.innerHTML = ""
-// })
