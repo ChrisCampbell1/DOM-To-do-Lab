a6b7d2d2e9e42a421521b3460639b520427689ad
Chris Campbell <christopherecampbell@gmail.com>
2022-12-06 22:08:19 -0700

    working on click to delete

--- js/app.js
+++ js/app.js
@@ -3,14 +3,16 @@ const inputElement = document.querySelector("#input")
 const buttonElement = document.querySelector("#submit-button")
 const ulElement = document.querySelector("#todo-list")
 const resetElement = document.querySelector("#reset-button")
+//const todoItem = document.querySelectorAll(".todo-item")
 
 //console.log tests
-console.log(inputElement)
+console.log()
 
 
 //event listeners
 buttonElement.addEventListener("click", function(){
   const liElement = document.createElement("li")
+  liElement.setAttribute("class", "todo-item")
   liElement.textContent = inputElement.value
   if(liElement.textContent){
   ulElement.appendChild(liElement)
@@ -22,3 +24,8 @@ resetElement.addEventListener("click", function(){
   ulElement.innerHTML = ""
   inputElement.value = ""
 })
+
+// can't get individual LIs to respond to the event listener, only the whole UL
+// ulElement.addEventListener("click", function(){
+//   ulElement.child.innerHTML = ""
+// })
